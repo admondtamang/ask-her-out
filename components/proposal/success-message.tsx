16abd3a5459503d6f339dc646402aa9d6d2a684e
admond tamang <admondtamang@gmail.com>
2024-12-22 23:37:17 +1100

    fix: confetti added

--- components/proposal/success-message.tsx
+++ components/proposal/success-message.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { CatGif } from "./cat-gif";
-import { HeartIcon } from "./heart-icon";
+import Realistic from "react-canvas-confetti/dist/presets/realistic";
 
 export function SuccessMessage() {
   const gifUrl = "https://i.imgur.com/6eED710.gif";
   return (
     <div className="text-center space-y-6">
+      <Realistic autorun={{ speed: 0.3, duration: 3 }} />
+
       <CatGif src={gifUrl} />
       <h2 className="text-4xl font-bold text-pink-600">
         Yay! I'm so happy! 💖
