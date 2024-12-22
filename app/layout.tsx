6cbbb66b09f15512c16b8a729db1cabb0f699669
admond tamang <admondtamang@gmail.com>
2024-12-23 01:10:14 +1100

    fix: clarity intalled

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "phznmg4p7m");
+              `,
+        }}
+      />
       <Toaster />
     </html>
   );
