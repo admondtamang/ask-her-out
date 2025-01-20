3d6b25d70c4d800dcd614d9f09df09b850368597
admond tamang <admondtamang@gmail.com>
2025-01-20 22:38:08 +0545

    fix: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
+import Clarity from '@microsoft/clarity';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,26 +12,18 @@ export const metadata: Metadata = {
   description: "App to ask her out",
 };
 
+Clarity.init("phznmg4p7m");
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-                (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "phznmg4p7m");
-              `,
-        }}
-      />
+      <Analytics />
       <Toaster />
     </html>
   );
